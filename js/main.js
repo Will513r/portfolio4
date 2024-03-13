7e2ad540e33a2f7eed42a3598d9a5a19fa34682e
William <will513r@gmail.com>
2024-03-13 09:36:45 -0400

    Updating and fixing bugs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-var typed = new Typed(".typing",{
-    strings:["","Web Developer", "Problem Solver", "Leader", "Father", "Motivator"],
-    typeSpeed:100,
-    backSpeed:60,
-    loop:true
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Developer",
+    "Problem Solver",
+    "Leader",
+    "Father",
+    "Motivator",
+  ],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
 });
