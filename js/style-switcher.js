805cee7cd0943e1a2d3f0ac92beaeda000e8ce5e
William <will513r@gmail.com>
2024-05-06 13:56:57 -0400

    light mode  added will to menu

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -34,14 +34,14 @@ const dayNight = document.querySelector(".day-night");
 //   }
 // };
 dayNight.addEventListener("click", () => {
-  dayNight.querySelector("i").classList.toggle("fa-sun");
   dayNight.querySelector("i").classList.toggle("fa-moon");
+  dayNight.querySelector("i").classList.toggle("fa-sun");
   document.body.classList.toggle("light");
 });
 window.addEventListener("load", () => {
   if (document.body.classList.contains("light")) {
-    dayNight.querySelector("i").classList.add("fa-moon");
-  } else {
     dayNight.querySelector("i").classList.add("fa-sun");
+  } else {
+    dayNight.querySelector("i").classList.add("fa-moon");
   }
 });
